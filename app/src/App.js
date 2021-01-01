4f3a5cb128ef152dcc4638a64d18ad0fa6ac237f
vpvnguyen <vpvnguyen@gmail.com>
2021-01-01 12:32:21 -0700

    remove TailwindFigure and renamed TodoApp to TodoPagination

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Landing from "./components/Landing";
 import Form from "./components/Form";
-import TailwindFigure from "./components/TailwindFigure";
-import TodoApp from "./components/TodoApp";
+import TodoPagination from "./components/TodoPagination";
 
 function App() {
   return (
     <>
       <Landing />
       <Form />
-      {/* <TailwindFigure /> */}
-      <TodoApp />
+      <TodoPagination />
     </>
   );
 }
