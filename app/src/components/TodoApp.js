e634e74e08fe6525159be88da0067bfdc0dce348
vpvnguyen <vpvnguyen@gmail.com>
2020-12-31 21:08:12 -0700

    extracted getting page numbers; log each variable used to determine pagination

--- app/src/components/TodoApp.js
+++ app/src/components/TodoApp.js
@@ -14,42 +14,39 @@ const style = {
 };
 
 const TodoApp = () => {
-  const [todos, setTodos] = useState([
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-  ]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [todosPerPage, setTodosPerPage] = useState(3);
+  const todos = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"];
+  const todosPerPage = 3;
+
+  // Logic for displaying page numbers
+  const getPageNumbers = () => {
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(todos.length / todosPerPage); i++) {
+      pageNumbers.push(i);
+    }
+    return pageNumbers;
+  };
 
   const handleClick = (event) => {
     setCurrentPage(Number(event.target.id));
   };
 
   // Logic for displaying current todos
-  const indexOfLastTodo = currentPage * todosPerPage;
+  const indexOfLastTodo = currentPage * todosPerPage; // 1 * 3 = 3; 3 * 3 = 9
+  console.log(`indexOfLastTodo: ${indexOfLastTodo}`);
   const indexOfFirstTodo = indexOfLastTodo - todosPerPage;
+  console.log(`indexOfFirstTodo: ${indexOfFirstTodo}`);
   const currentTodos = todos.slice(indexOfFirstTodo, indexOfLastTodo);
+  console.log(`currentTodos: ${currentTodos}`);
+  const pageNumbers = getPageNumbers();
 
   const renderTodos = currentTodos.map((todo, index) => {
+    console.log("renderTodos");
     return <li key={index}>{todo}</li>;
   });
 
-  // Logic for displaying page numbers
-  const pageNumbers = [];
-  for (let i = 1; i <= Math.ceil(todos.length / todosPerPage); i++) {
-    pageNumbers.push(i);
-  }
-
   const renderPageNumbers = pageNumbers.map((number) => {
+    console.log("renderPageNumbers");
     return (
       <li key={number} id={number} style={style.pageItem} onClick={handleClick}>
         {number}
