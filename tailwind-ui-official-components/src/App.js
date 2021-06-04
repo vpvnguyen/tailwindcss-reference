3f71db6dbe1bde1e9c02a06336d2c56bdd4b3153
Vincent Nguyen <vpvnguyen@gmail.com>
2021-06-03 22:19:27 -0700

    install react and configured tailwind

---
+++ tailwind-ui-official-components/src/App.js
@@ -0,0 +1,65 @@
+import HeroSection from "./PageSections/HeroSection";
+import FeatureSection from "./PageSections/FeatureSection";
+import CTASection from "./PageSections/CTASection";
+
+import Header from "./Elements/Header";
+import Banner from "./Elements/Banner";
+import Avatar from "./Elements/Avatar";
+import SimpleDropdown from "./Elements/SimpleDropdown";
+import DividerDropdown from "./Elements/DividerDropdown";
+
+import StackedLayout from "./ApplicationShells/StackedLayout";
+
+import PageHeading from "./Headings/PageHeading";
+
+import DescriptionList from "./DataDisplays/DescriptionList";
+
+import Table from "./Lists/Table";
+
+import FormLayout from "./Forms/FormLayout";
+import InputGroup from "./Forms/InputGroup";
+import SelectMenu from "./Forms/SelectMenu";
+import SignIn from "./Forms/SignIn";
+
+import Navbar from "./Navigation/Navbar";
+import Pagination from "./Navigation/Pagination";
+
+import Modal from "./Overlays/Modal";
+import SlideOver from "./Overlays/SlideOver";
+
+function App() {
+  return (
+    <div>
+      {/* <HeroSection /> */}
+      {/* <FeatureSection /> */}
+      {/* <CTASection /> */}
+
+      <Header />
+      {/* <Banner /> */}
+      {/* <Avatar /> */}
+      {/* <SimpleDropdown /> */}
+      {/* <DividerDropdown /> */}
+
+      {/* <StackedLayout /> */}
+
+      {/* <PageHeading /> */}
+
+      {/* <DescriptionList /> */}
+
+      {/* <Table /> */}
+
+      {/* <FormLayout /> */}
+      {/* <InputGroup /> */}
+      {/* <SelectMenu /> */}
+      {/* <SignIn /> */}
+
+      {/* <Navbar /> */}
+      {/* <Pagination /> */}
+
+      {/* <Modal /> */}
+      {/* <SlideOver /> */}
+    </div>
+  );
+}
+
+export default App;
